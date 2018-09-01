1303a6d680f88af6856c94b4aff06e97f1daceef
jingxuansd <360244939@qq.com>
2018-09-01 12:30:19 +0800

    first commit

---
+++ src/main/java/com/jx/esclient/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.jx.esclient.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+    private static Logger logger = LoggerFactory.getILoggerFactory().getLogger("DemoApplication.class");
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        logger.info("dev");
+    }
+}
---
+++ src/test/java/com/jx/esclient/demo/DemoApplicationTests.java
@@ -0,0 +1,20 @@
+package com.jx.esclient.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+    Logger logger = LoggerFactory.getILoggerFactory().getLogger("DemoApplicationTests.class");
+
+    @Test
+    public void contextLoads() {
+        logger.info("test");
+    }
+
+}
