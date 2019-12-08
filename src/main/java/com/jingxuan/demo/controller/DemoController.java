0c368b9a7ea66ba4e8af06905a8eefe38b3f77c7
Xuan Jing <jingxuan@jingxuandeMacBook-Pro.local>
2019-12-08 22:17:15 +0800

    add map merge test

--- src/main/java/com/jingxuan/demo/controller/DemoController.java
+++ src/main/java/com/jingxuan/demo/controller/DemoController.java
@@ -4,12 +4,23 @@ package com.jingxuan.demo.controller;
 import com.jingxuan.demo.model.User;
 import com.jingxuan.demo.servie.IUserService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.math.BigDecimal;
+import java.text.MessageFormat;
+import java.util.*;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.reducing;
+import static java.util.stream.Collectors.toList;
 
 /**
  * @author Xuan Jing
@@ -26,9 +37,59 @@ public class DemoController {
     String getName(@PathVariable("id") int id, Model model) {
         User user = userService.getUserById(id);
         String name = user.getName();
-        log.info("param: name:{}", name);
+        log.info("name:{}", name);
         model.addAttribute("name", name);
+
+        test(model);
+
         return "index";
     }
 
+    private void test(Model model) {
+        List<User> users = new ArrayList<>();
+        users.add(User
+                .builder()
+                .name("jingxuan")
+                .email("123@qq.com").amount(BigDecimal.TEN).build());
+        users.add(User
+                .builder()
+                .name("jingxuan")
+                .email("123@qq.com").amount(BigDecimal.TEN).build());
+        users.add(User
+                .builder()
+                .name("jingxuan")
+                .email("234@qq.com").amount(BigDecimal.TEN).build());
+        users.add(User
+                .builder()
+                .name("jingxuan")
+                .email("234@qq.com").amount(BigDecimal.TEN).build());
+
+
+        Map<String, User> mergeUserMap = new HashMap<>();
+        users.forEach(user ->
+            mergeUserMap.merge(
+                    getKey(user),
+                    user,
+                    this::mergeUser)
+        );
+        model.addAttribute("test",
+                MessageFormat.format("map size: {0}, map key: {1}, map value: {2}",
+                        mergeUserMap.size(), mergeUserMap.keySet(), mergeUserMap.values()));
+        log.info("merge users: [{}]", mergeUserMap);
+    }
+
+    private String getKey(User user) {
+        return String.join("#",user.getName(),user.getEmail());
+    }
+
+    private User mergeUser(User u1, User u2) {
+        if (u1 == null) {
+            return u2;
+        }
+        if (u2 == null) {
+            return u1;
+        }
+        u1.setAmount(u1.getAmount().add(u2.getAmount()));
+        return u1;
+    }
 }
--- src/main/java/com/jingxuan/demo/model/User.java
+++ src/main/java/com/jingxuan/demo/model/User.java
@@ -1,12 +1,25 @@
 package com.jingxuan.demo.model;
 
-import lombok.Data;
+import lombok.*;
+import org.mapstruct.TargetType;
 
-@Data
+import java.math.BigDecimal;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     private Integer id;
 
+    private Long uid;
+
     private String name;
 
-    private Long uid;
+    private String email;
+
+    private String address;
+
+    private BigDecimal amount;
 }
