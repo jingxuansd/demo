ed9488877afc4fd55b430630a0cf6851b8e094a6
jingxuan <jingxuan@xsyxsc.com>
2023-01-06 18:04:13 +0800

    update

--- src/test/java/com/jingxuan/demo/BaseMapperTest.java
+++ src/test/java/com/jingxuan/demo/BaseMapperTest.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.ZoneId;
@@ -26,7 +27,7 @@ import static com.wix.mysql.distribution.Version.v5_7_17;
  */
 @ActiveProfiles("test")
 @SpringBootTest
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 public class BaseMapperTest {
     private static EmbeddedMysql embeddedMysql;
 
